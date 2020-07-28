b33388576df4d03813e5a1d74ba15f146b1747af
lemonWM <arturchochla@gmail.com>
2020-07-28 20:55:46 +0200

    make it better

--- pages/user/_id.vue
+++ pages/user/_id.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-        <p>{{user}}</p>
+        <p>{{user.name}}</p>
     </div>
 </template>
 
 <script>
 export default {
     name: 'user',
-    props: ['user']
+    props: ['user'],
+    watch: {
+        user(n){
+            console.log(n)
+        }
+    }
 }
 </script>
