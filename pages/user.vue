3cbbcf21c689d063d945c70a2f05aba3dc7ea83f
lemonWM <arturchochla@gmail.com>
2020-07-28 19:45:09 +0200

    make it better

---
+++ pages/user.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+       <div v-for="(user, index) in users">
+            <nuxt-link :to="{name: 'single-user', params: {id: user.name, user: user}}">{{user.name}}</nuxt-link>
+       </div>
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+    name: 'user',
+    data() {
+        return {
+            user: 'Jan',
+            users: []
+        }
+    },
+    created() {
+        axios.get(`https://cantus-app.herokuapp.com/categories`)
+        .then(({ data})=> {
+            
+            this.call(data)
+            this.users = data
+        })
+    },
+    methods: {
+        call(data){
+            console.log(data)
+        }
+    }
+}
+</script>
