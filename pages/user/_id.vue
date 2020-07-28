fe82457299c4a6827e0bec9cad11cd71ad467bfb
lemonWM <arturchochla@gmail.com>
2020-07-28 21:28:22 +0200

    make it better

--- pages/user/_id.vue
+++ pages/user/_id.vue
@@ -1,13 +1,23 @@
 <template>
     <div>
         <p>name</p>
-        <p>{{current.name}}</p>
+        <p>{{user.name}}</p>
     </div>
 </template>
 
 <script>
 export default {
     name: 'user',
-    props: ['current']
+    //props: ['current'],
+    data() {
+        return {
+            user: {}
+        }
+    },
+       mounted() {
+        console.log(this.$route
+        )
+        this.user = this.$route.params.current
+      }
 }
 </script>
