0c858a7095c9e5fe58fbd94c43c60f8de92823d2
lemonWM <arturchochla@gmail.com>
2020-08-08 10:23:14 +0200

    Update content.vue

--- components/forSale/content.vue
+++ components/forSale/content.vue
@@ -1,26 +1,28 @@
 <template>
     <div class="sales-wrapper row">
-        <div class="single-wrapper columns small-4" v-for="(single, index) in sales">
+        <div class="single-wrapper columns small-4" v-for="(single, index) in sales" :key="single._id">
             <div class="single-sale-element ">
-                <div class="img-element">
-                    <figure>
-                        <img :src="single.photos[0]" alt="">
-                    </figure>               
-                </div>
-                <div>
-                    <h3>${{single.price}}</h3>
-                </div>
-                <div>
-                    <p>{{single.address}}</p>
-                </div>
-                <div>
-                    <p>{{single.schedulePlace.bedrooms}} bedrooms / {{single.schedulePlace.bathrooms}} bathrooms / {{single.totalArea}} &sup2</p>
-                </div>
+                <article @click='dd(single._id)' class="element-sale">
+                    <div class="img-element">
+                        <figure>
+                            <img :src="single.photos[0]" alt="" class="img-single">
+                        </figure>               
+                    </div>
+                    <div>
+                        <h3 class="price">${{single.price}}</h3>
+                    </div>
+                    <div>
+                        <p class="address">{{single.address}}</p>
+                    </div>
+                    <div>
+                        <p class="details">{{single.schedulePlace.bedrooms}} bedrooms / {{single.schedulePlace.bathrooms}} bathrooms / {{single.totalArea}} &sup2</p>
+                    </div>          
+                </article>
                 <div class="add-to-observe">
                     <button>
-                        <i class="far fa-heart"></i>
+                        <i class="far fa-heart add-to-observe-icon"></i>
                     </button>
-                </div>
+                </div>  
             </div>
         </div>
     </div>
@@ -65,7 +67,62 @@ export default {
             this.$store.commit('setSales', {
                 sales: data
             })
+        },
+        dd(id){
+            console.log(id)
+            // go to single
         }
     },
 }
 </script>
+
+<style scoped>
+
+.sales-wrapper{
+    padding-top: 30px;
+}
+.element-sale{
+    cursor: pointer;
+}
+.single-wrapper{
+    background-color: #505767;
+    margin: 20px;
+    border-radius: 10px;
+    border-radius: 30px;
+}
+
+.single-sale-element{
+    padding-top: 15px;
+    padding-bottom: 10px;
+    position: relative;
+    
+}
+.img-single{
+    border-radius: 20px;
+}
+.price{
+    color: #c2f9df;
+    font-size: 24px;
+}
+.address{
+    color: #86959c;
+    margin-bottom: 5px;
+}
+.details{
+    font-size: 14px;
+    margin-bottom: 5px;
+    color: #86959c;
+}
+.add-to-observe{
+    position: absolute;
+    top: 26px;
+    right: 10px;
+    background-color: #505767;
+    padding: 10px 15px;
+    border-radius: 15px;   
+}
+.add-to-observe-icon{
+    color: #c2f9df;
+    font-size: 18px;
+}
+</style>
