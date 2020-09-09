1b737f975017abff0069ddf389cd91869022ad1e
lemonWM <arturchochla@gmail.com>
2020-09-09 19:38:36 +0200

    Update singleDescribe.vue

--- components/forSale/single/singleDescribe.vue
+++ components/forSale/single/singleDescribe.vue
@@ -2,11 +2,11 @@
     <div v-if="single">
         <div class="details-wrapper">
             <div class="details-element">
-                <div class="head-line">
-                    <h2 class="title" @click="showElement(1)">Overview</h2>
-                    <h2 class="title" @click="showElement(2)">Features</h2>
-                    <h2 class="title" @click="showElement(3)">House value</h2>
-                    <h2 class="title" @click="showElement(4)">Address</h2>
+                <div class="head-line row">
+                    <h2 class="title" @click="showElement(1)" v-bind:class="{'active': show == 1}">Overview</h2>
+                    <h2 class="title" @click="showElement(2)" v-bind:class="{'active': show == 2}">Features</h2>
+                    <h2 class="title" @click="showElement(3)" v-bind:class="{'active': show == 3}">House value</h2>
+                    <h2 class="title" @click="showElement(4)" v-bind:class="{'active': show == 4}">Address</h2>
                 </div>
                 <div class="text-main">
                     <div class="text-wrapper" v-if="show == 1">
@@ -48,7 +48,6 @@ export default {
             this.show = value
         }
     },
-
 }
 </script>
 
@@ -56,6 +55,19 @@ export default {
 
 .title{
     cursor: pointer;
+    font-size: 20px;
+    margin-right: 20px;
+    margin-top: 20px;
+    padding-bottom: 10px;
+    border-bottom: 1px solid transparent;
 }
-
+.active{
+    color: #af9668;
+    border-bottom: 1px solid #af9668;
+}
+.text-wrapper{
+    max-width: 600px;
+    color: #86959c;
+    margin-top: 20px;   
+}
 </style>
