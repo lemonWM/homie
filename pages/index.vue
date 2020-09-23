97e5fc9759129337f01ea22bd3f82ef08f864e9f
lemonWM <arturchochla@gmail.com>
2020-09-23 14:46:21 +0200

    make it better

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,37 @@
 <template>
-    <div>
-      <homeHeader />
-    </div>
+<div>
+    <homeHeader />
+</div>
 </template>
 
 <script>
-
 import homeHeader from '../components/home/header'
 
 export default {
-  name: 'home',
-  data() {
-    return {
-      error: false
+    name: 'home',
+    data() {
+        return {
+            error: false
+        }
+    },
+    created() {
+        this.$axios.get(`${this.$axios.defaults.baseURL}/run`)
+        // test api for runing free heroku server
+    },
+    components: {
+        homeHeader
     }
-  },
-  components:{
-    homeHeader
-  }
 }
 </script>
 
-
 <style>
-h1,h2,h3,p,span,a,button{
-  font-family: 'Abel', sans-serif;
+h1,
+h2,
+h3,
+p,
+span,
+a,
+button {
+    font-family: 'Abel', sans-serif;
 }
 </style>
