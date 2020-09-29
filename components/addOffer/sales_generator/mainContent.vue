5bd4f75b91216fab25e2c327fb1830b9b01af316
lemonWM <arturchochla@gmail.com>
2020-09-29 14:26:13 +0200

    sels new module

--- components/addOffer/sales_generator/mainContent.vue
+++ components/addOffer/sales_generator/mainContent.vue
@@ -1,65 +1,45 @@
 <template>
     <div class="item-new">
-        <div>
-            <label for="">Select address</label>
-            <vue-google-autocomplete
-                id="map"
-                ref="addresName"
-                classname="input-sels"
-                placeholder="Enter city, ex. 'Los Angeles'"
-                v-on:placechanged="getToData"
-                types="(cities)"
-                country="" 
-            />
+        <div class="module">
+            <div class="row">
+                <h2>Address information</h2>
+                <button class="hollow" @click="openAddress =! openAddress">
+                    <i class="fas fa-chevron-down" v-if="openAddress"></i>
+                    <i class="fas fa-chevron-up" v-else></i>
+                </button>
+            </div>
+            <addressContent v-if="openAddress"/>
         </div>
-        <div>
-            <label for="street">Street name</label>
-            <input type="text" id="street" placeholder="Write name of street" class="input-sels">
+
+        <div class="module">
+            <div class="row">
+                <h2>Details</h2>
+                <button class="hollow" @click="openDetails =! openDetails">
+                    <i class="fas fa-chevron-down" v-if="openDetails"></i>
+                    <i class="fas fa-chevron-up" v-else></i>
+                </button>
+            </div>
         </div>
-        <div>
-            <label for="streetNumber">Numer on street</label>
-            <input type="text" id="street" placeholder="Write numer on street" class="input-sels">
-        </div>
-        <div>
-            <label for="zipcode">Type zipcode</label>
-            <input type="number" id="zipcode" placeholder="Write zipcode" class="input-sels">
-        </div>
+
     </div>
 </template>
 
 <script>
 
 import VueGoogleAutocomplete from '../../../node_modules/vue-google-autocomplete'
+import addressContent from './addressContent'
 
 export default {
     name: 'new-sale-generator',
     data() {
         return {
-            addresName:''
+            openAddress: true,
+            openDetails: true
         }
     },
-    mounted() {
-
-        this.$refs.addresName.focus();
-    },
-    methods: {
-               /**
-        * When the location found
-        * @param {Object} addressData Data of the found location
-        * @param {Object} placeResultData PlaceResult object
-        * @param {String} id Input container ID
-        */
-        getToData: function (addressData, placeResultData, id) {
-
-            console.log(addressData)
-            
-        },
-    },
-    watch: {
-        addresName(newLocalization){
-            console.log(newLocalization)
-        }
-    },
-    components: { VueGoogleAutocomplete }
+    components: { 
+        VueGoogleAutocomplete,
+        addressContent
+     }
 }
 </script>
