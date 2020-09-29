80e315c7b7f8e607dc35b81d9e9857b8edea18a6
lemonWM <arturchochla@gmail.com>
2020-09-29 15:39:15 +0200

    Update photosContent.vue

--- components/addOffer/sales_generator/photosContent.vue
+++ components/addOffer/sales_generator/photosContent.vue
@@ -1,11 +1,56 @@
 <template>
     <div class="item-new">
-    
+        <input type="file" 
+            accept="image/png, image/jpeg, image/bmp"
+            ref="file"
+            @change="handleFileUpload()"
+            class="input-sels"
+            >
+        <button class="btn btn-primary" 
+            @click="uploadImg" 
+            >
+             <i class="fa fa-cloud-upload-alt"></i>
+        </button>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'photos'
+    name: 'photos',
+    data() {
+        return {
+            imageData: '',
+            logoUrl:''
+        }
+    },
+    methods: {
+        
+        uploadImg(){
+            
+            let formData = new FormData()
+
+            formData.append('file', this.imageData)
+
+            this.$axios.post(this.$axios.defaults.baseURL+'/upload_img', 
+                formData,
+                {headers: 
+                    {"Content-Type": "multipart/form-data"}
+                }
+            )
+            .then(({ data }) => {
+                
+                let logoUrl = data.url
+            })
+            .catch(({ err }) => {
+                console.log(err)
+            })
+        },
+        handleFileUpload(){
+
+            this.imageData = this.$refs.file.files[0]
+        
+            console.log(this.imageData)
+        }, 
+    },
 }
 </script>
