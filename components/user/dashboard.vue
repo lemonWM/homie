b0cd4b767d3b706335631aaca8a777942bb5e147
lemonWM <arturchochla@gmail.com>
2020-11-09 19:31:13 +0100

    style user panel

--- components/user/dashboard.vue
+++ components/user/dashboard.vue
@@ -6,9 +6,22 @@
                 <img :src="user.logo" alt="../../static/user-logo.png">
                 <h2>{{user.user}}</h2>
             </div>
+            <div class="user-main">
+                <h3>Contact options</h3>
+                <div class="contact-wrapper">
+                    <a :href="`tel:${user.phone}`">
+                        <i class="fas fa-mobile"></i>
+                        {{user.phone}}
+                    </a>
+                    <a :href="`mailto:${user.email}`">
+                        <i class="fas fa-envelope-open"></i>
+                        {{user.email}}
+                    </a>   
+                </div>
+            </div>
             <p>User name: {{user.user}}</p>
         </div>
-       
+       <pre>{{user}}</pre>
     </div>
 </template>
 
