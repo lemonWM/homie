70bab1f00ae3bf33d8fcb0de110e89eb69f1401b
lemonWM <arturchochla@gmail.com>
2020-11-09 19:08:08 +0100

    style on user panel

--- components/user/dashboard.vue
+++ components/user/dashboard.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="section-content dashboard">
-        
+        <h3>User information</h3>
+        <div class="user-information-wrapper">
+            <div class="user-header row">
+                <img :src="user.logo" alt="../../static/user-logo.png">
+                <h2>{{user.user}}</h2>
+            </div>
+            <p>User name: {{user.user}}</p>
+        </div>
+       
     </div>
 </template>
 
