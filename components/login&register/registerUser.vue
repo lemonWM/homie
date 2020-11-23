48bd2217011f418db8ef5effcbc7bcb764231b41
lemonWM <arturchochla@gmail.com>
2020-11-23 21:27:16 +0100

    vuealidate register fix

--- components/login&register/registerUser.vue
+++ components/login&register/registerUser.vue
@@ -10,37 +10,48 @@
                         <div class="medium-3 cell">
                             <label>
                                 <i class="fas fa-user"></i>
-                                <input type="text" placeholder="LOGIN" v-model.trim="user" >
-                                <p class="form-input-hint" v-if="!$v.user.required">Login is required</p>
+                                <input type="text" placeholder="LOGIN" v-model="user" >
+                                <p class="form-input-hint" v-if="$v.user.$dirty && !$v.user.required">Login is required</p>
+                                <p class="form-input-hint" v-if="! $v.user.$pending && !$v.user.unique">This user is already in use</p>
                             </label>
                         </div>
                         <div class="medium-3 cell">
                             <label>
                                 <i class="fas fa-user"></i>
-                                <input type="text" placeholder="FIRST NAME" v-model.trim="firstName" >
-                                <p class="form-input-hint" v-if="!$v.firstName.required">First name is required</p>
+                                <input type="text" placeholder="FIRST NAME" v-model.lazy="firstName" >
+                                <p class="form-input-hint" v-if="$v.firstName.$dirty && !$v.firstName.required">First name is required</p>
                             </label>
                         </div>
                         <div class="medium-3 cell">
                             <label>
                                 <i class="fas fa-user"></i>
-                                <input type="text" placeholder="LAST NAME" v-model.trim="lastName" >
-                                <p class="form-input-hint" v-if="!$v.lastName.required">Last name is required</p>
+                                <input type="text" placeholder="LAST NAME" v-model.lazy="lastName" >
+                                <p class="form-input-hint" v-if="$v.lastName.$dirty && !$v.lastName.required">Last name is required</p>
                             </label>
                         </div>
                         <div class="medium-3 cell">
                             <label>
                                 <i class="fas fa-envelope"></i>
                                 <input type="text" placeholder="EMAIL" v-model.lazy="email" @input="$v.email.$touch()">
-                                <p class="form-input-hint" v-if="!$v.email.required">E-mail address required</p>
-                                <p class="form-input-hint" v-if="!$v.email.email">Incorrect email form</p>
+                                <p class="form-input-hint" v-if="$v.email.$dirty && !$v.email.required">E-mail address required</p>
+                                <p class="form-input-hint" v-if="$v.email.$dirty && !$v.email.email">Incorrect email form</p>
                             </label>
                         </div>
                         <div class="medium-3 cell">
                             <label>
                                 <i class="fas fa-key"></i>
                                 <input type="password" placeholder="PASSWORD" v-model="password" @blur="$v.password.$touch()">
-                                <p class="form-input-hint" v-if="!$v.password.minLength">Required min. 5 signs</p>
+                                <p class="form-input-hint" v-if="$v.password.$dirty && !$v.password.minLength">Required min. 5 signs</p>
+                                <p class="form-input-hint" v-if="$v.password.$dirty && !$v.password.containsUppercase">Need to use upper case sign</p>
+                                <p class="form-input-hint" v-if="$v.password.$dirty && !$v.password.containsLowercase">Need to use lower case sign</p>
+                                <p class="form-input-hint" v-if="$v.password.$dirty && !$v.password.containsNumber">Need to use number sign</p>
+                            </label>
+                        </div>
+                        <div class="medium-3 cell">
+                            <label>
+                                <i class="fas fa-key"></i>
+                                <input type="password" placeholder="PASSWORD" v-model="passwordConfirm" @blur="$v.passwordConfirm.$touch()">
+                                <p class="form-input-hint" v-if="$v.passwordConfirm.$dirty && !$v.passwordConfirm.sameAs">Passwords are differents</p>
                             </label>
                         </div>
                     <p v-if="$v.$invalid == false">You complited required basic!<br> You want to update more profile details?</p>
@@ -69,17 +80,15 @@
             </form>      
         </div>
 
-        
-
-
         <button type="button" class=" button-generate" :disabled='$v.$invalid' @click="register">SIGN IN</button>
+
     </div>
 </template>
 
 <script>
 
 import { validationMixin } from 'vuelidate';
-import { required,  minLength, email} from 'vuelidate/lib/validators';
+import { required,  minLength, email, sameAs} from 'vuelidate/lib/validators';
 
 export default {
     name: 'register-user-panel',
@@ -90,6 +99,7 @@ export default {
             firstName:'',
             lastName:'',
             password: '',
+            passwordConfirm:'',
             email:'',
             phone: '',
             logo: '',
@@ -130,25 +140,64 @@ export default {
 
             .then(({ data })=>{
 
-                console.log(data)
+                this.setLogged(data)
+
+                this.$router.push({name: 'index'})
             })
             .catch(({ data })=>{
 
                 this.error = data
             })
+        },
+        setLogged(user) {
+
+            this.$store.commit('loginUser', {
+                user: user
+            })
         }
     },
     validations: {
         user: {
-            required
+            required,
+            unique(value){
+                
+                if( value === ''){
+                    
+                    return true
+
+                } else {
+                
+                return this.$axios.post('/validate-user-login', {
+                            
+                           user: value
+                        })            
+                        .then(response => {
+                      
+                            return response.data.unique             
+                        })
+                }  
+            }
         },
         password: {
             required,
-            minLength: minLength(5)
+            minLength: minLength(5),
+            containsUppercase: function(value) {
+            return /[A-Z]/.test(value)
+            },
+            containsLowercase: function(value) {
+            return /[a-z]/.test(value)
+            },
+            containsNumber: function(value) {
+            return /[0-9]/.test(value)
+            }
+        },
+        passwordConfirm: {
+            required,
+            sameAs: sameAs('password')
         },
         email: {
             required,
-            email
+            email,
         },
         firstName:{
             required
